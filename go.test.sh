45ee5556f2c518ee79ac31eb2362e5197bd62721
Romain Champourlier <public@rchampourlier.com>
2018-07-28 18:42:52 +0200

    Fixing coverage config

--- go.test.sh
+++ go.test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 go test -v -covermode=count -coverprofile=coverage.out
-$GOPATH/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
+$GOPATH/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
