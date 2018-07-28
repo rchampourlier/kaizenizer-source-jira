e990c962681213eaf4792f1c371bbf74fa3c6cf4
Romain Champourlier <public@rchampourlier.com>
2018-07-28 18:37:28 +0200

    Fixed goveralls path

--- go.test.sh
+++ go.test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 go test -v -covermode=count -coverprofile=coverage.out
-goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
+$GOPATH/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
