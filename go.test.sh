36cdca68118268763cd5b02037bebe17842affc6
Romain Champourlier <public@rchampourlier.com>
2018-07-28 18:45:27 +0200

    Fixed go.test.sh

--- go.test.sh
+++ go.test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-go test -v -covermode=count -coverprofile=coverage.out
+go test -v -covermode=count -coverprofile=coverage.out ./...
 $GOPATH/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
