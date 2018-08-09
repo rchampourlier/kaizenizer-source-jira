bd03650450ad1473ae86c356c0909b367eab16ba
Romain Champourlier <public@rchampourlier.com>
2018-08-09 10:12:24 +0200

    Reworked `jira/sync_test.go`
    - Refactored test and issue mocking to ease test changes.

--- jira/sync_test.go
+++ jira/sync_test.go
@@ -17,21 +17,53 @@ import (
 // `jira/mapping.go` is not unit tested but its tested in integration
 // through these tests.
 
+type issueMockDef struct {
+	key        string
+	refTime    time.Time
+	assignee   *string // initial assignee, will be modified by changelogs
+	status     string  // initial status, will be modified by changelogs
+	changelogs []struct {
+		field string
+		from  string
+		to    string
+	}
+}
+
 func TestPerformIncrementalSync(t *testing.T) {
-	issueMocks := []struct {
-		key        string
-		changelogs []string
-	}{
-		{"PJ-1", []string{}},
-		{"PJ-2", []string{"status", "Open", "In Dev"}},
-		{"PJ-3", []string{"assignee", "Me", "You"}},
+	refTime := time.Now()
+	assigneeName := "PM"
+	issueMockDefs := []issueMockDef{
+		issueMockDef{
+			"PJ-1",
+			refTime,
+			&assigneeName,
+			"Open",
+			[]struct{ field, from, to string }{},
+		},
+		issueMockDef{
+			"PJ-2",
+			refTime,
+			nil,
+			"Open",
+			[]struct{ field, from, to string }{
+				{"status", "Open", "In Dev"},
+				{"assignee", "PM", "Dev"},
+			},
+		},
+		issueMockDef{
+			"PJ-3",
+			refTime,
+			&assigneeName,
+			"In Dev",
+			[]struct{ field, from, to string }{
+				{"assignee", "Me", "You"},
+			},
+		},
 	}
 	var issueKeys []string
-	for _, issueMock := range issueMocks {
-		issueKeys = append(issueKeys, issueMock.key)
+	for _, issueMockDef := range issueMockDefs {
+		issueKeys = append(issueKeys, issueMockDef.key)
 	}
-
-	refTime := time.Now()
 
 	c := client.NewMockClient(t)
 	s := store.NewMockStore(t)
@@ -44,16 +76,16 @@ func TestPerformIncrementalSync(t *testing.T) {
 		WillRespondWithIssueKeys(issueKeys)
 
 	// for each issue in the search results
-	for _, im := range issueMocks {
-		i, is, ies := mockIssue(im.key, refTime, im.changelogs)
+	for _, def := range issueMockDefs {
+		i, is, ies := mockIssue(def)
 
 		// perform a get issue
-		c.ExpectGetIssue(im.key).WillRespondWithIssue(i)
+		c.ExpectGetIssue(def.key).WillRespondWithIssue(i)
 
 		// replace previous records of the issue with new
 		//   state and events records
 		s.ExpectReplaceIssueStateAndEvents().
-			WithIssueKey(im.key).
+			WithIssueKey(def.key).
 			WithIssueState(is).
 			WithIssueEvents(ies).
 			WillReturnError(nil)
@@ -65,115 +97,84 @@ func TestPerformIncrementalSync(t *testing.T) {
 // mockIssue mocks a Jira issue. It returns the mocked `extJira.Issue` as well
 // as the corresponding `store.IssueState` and `store.IssueEvent`s that are to
 // be expected for this issue.
-func mockIssue(issueKey string, refTime time.Time, changelogs []string) (*extJira.Issue, *store.IssueState, []*store.IssueEvent) {
+func mockIssue(def issueMockDef) (*extJira.Issue, *store.IssueState, []*store.IssueEvent) {
 	issueFields := extJira.IssueFields{
 		Type:           extJira.IssueType{Name: "Bug"},
 		Project:        extJira.Project{Key: "PJ", Name: "Project"},
 		Status:         &extJira.Status{Name: "In Review"},
-		Resolutiondate: extJira.Time(refTime),
-		Created:        extJira.Time(refTime),
-		Duedate:        extJira.Date(refTime),
-		Updated:        extJira.Time(refTime),
+		Resolutiondate: extJira.Time(def.refTime),
+		Created:        extJira.Time(def.refTime),
+		Duedate:        extJira.Date(def.refTime),
+		Updated:        extJira.Time(def.refTime),
 		Priority:       &extJira.Priority{Name: "Major"},
 		Summary:        "summary",
 		Description:    "description",
 	}
 	changelog := extJira.Changelog{}
-	if len(changelogs) > 0 {
-		switch changelogs[0] { // kind of changelog
-		case "assignee":
-			h := extJira.ChangelogHistory{
-				Author: extJira.User{
-					Name: "assignee_change_author",
+	for _, cl := range def.changelogs {
+		h := extJira.ChangelogHistory{
+			Author: extJira.User{
+				Name: fmt.Sprintf("%s_change_author", cl.field),
+			},
+			Created: timeAsStr(def.refTime),
+			Items: []extJira.ChangelogItems{
+				extJira.ChangelogItems{
+					Field:      cl.field,
+					FieldType:  "string",
+					From:       cl.from,
+					FromString: cl.from,
+					To:         cl.to,
+					ToString:   cl.to,
 				},
-				Created: timeAsStr(refTime),
-				Items: []extJira.ChangelogItems{
-					extJira.ChangelogItems{
-						Field:      "assignee",
-						FieldType:  "string",
-						From:       changelogs[1],
-						FromString: changelogs[1],
-						To:         changelogs[2],
-						ToString:   changelogs[2],
-					},
-				},
-			}
-			changelog.Histories = append(changelog.Histories, h)
-		case "status":
-			h := extJira.ChangelogHistory{
-				Author: extJira.User{
-					Name: "status_change_author",
-				},
-				Created: timeAsStr(refTime),
-				Items: []extJira.ChangelogItems{
-					extJira.ChangelogItems{
-						Field:      "status",
-						FieldType:  "string",
-						From:       changelogs[1],
-						FromString: changelogs[1],
-						To:         changelogs[2],
-						ToString:   changelogs[2],
-					},
-				},
-			}
-			changelog.Histories = append(changelog.Histories, h)
-		default:
+			},
 		}
+		changelog.Histories = append(changelog.Histories, h)
 	}
 
 	i := &extJira.Issue{
-		Key:       issueKey,
+		Key:       def.key,
 		Fields:    &issueFields,
 		Changelog: &changelog,
 	}
 	is := &store.IssueState{
-		CreatedAt:   refTime,
-		UpdatedAt:   refTime,
-		Key:         issueKey,
+		CreatedAt:   def.refTime,
+		UpdatedAt:   def.refTime,
+		Key:         def.key,
 		Project:     strAddr("Project"),
-		ResolvedAt:  timeAddr(refTime),
+		ResolvedAt:  timeAddr(def.refTime),
 		Summary:     strAddr("summary"),
 		Description: strAddr("description"),
 	}
 	ies := []*store.IssueEvent{
 		{
-			EventTime:   refTime,
-			EventKind:   "created",
-			EventAuthor: "N/A",
-			// "N/A" because the issue's reporter is not set, so the required string is replaced by N/A
-			IssueKey:         issueKey,
-			CommentBody:      nil,
-			StatusChangeFrom: nil,
-			StatusChangeTo:   nil,
+			EventTime:          def.refTime,
+			EventKind:          "created",
+			EventAuthor:        "N/A", // the issue reporter is not set, required string is replaced by N/A
+			IssueKey:           def.key,
+			CommentBody:        nil,
+			StatusChangeFrom:   nil,
+			StatusChangeTo:     nil,
+			AssigneeChangeFrom: nil,
+			AssigneeChangeTo:   nil,
 		},
 	}
-	if len(changelogs) > 0 {
-		switch changelogs[0] {
-		case "assignee":
-			ies = append(ies, &store.IssueEvent{
-				EventTime:          refTime,
-				EventKind:          "assignee_changed",
-				EventAuthor:        "assignee_change_author",
-				IssueKey:           issueKey,
-				CommentBody:        nil,
-				StatusChangeFrom:   nil,
-				StatusChangeTo:     nil,
-				AssigneeChangeFrom: strAddr(changelogs[1]),
-				AssigneeChangeTo:   strAddr(changelogs[2]),
-			})
-		case "status":
-			ies = append(ies, &store.IssueEvent{
-				EventTime:          refTime,
-				EventKind:          "status_changed",
-				EventAuthor:        "status_change_author",
-				IssueKey:           issueKey,
-				CommentBody:        nil,
-				StatusChangeFrom:   strAddr(changelogs[1]),
-				StatusChangeTo:     strAddr(changelogs[2]),
-				AssigneeChangeFrom: nil,
-				AssigneeChangeTo:   nil,
-			})
+	for _, cl := range def.changelogs {
+		ie := &store.IssueEvent{
+			EventTime:   def.refTime,
+			EventKind:   fmt.Sprintf("%s_changed", cl.field),
+			EventAuthor: fmt.Sprintf("%s_change_author", cl.field),
+			IssueKey:    def.key,
+			CommentBody: nil,
 		}
+		switch cl.field {
+		case "status":
+			ie.StatusChangeFrom = strAddr(cl.from)
+			ie.StatusChangeTo = strAddr(cl.to)
+		case "assignee":
+			ie.AssigneeChangeFrom = strAddr(cl.from)
+			ie.AssigneeChangeTo = strAddr(cl.to)
+		}
+		ies = append(ies, ie)
 	}
 
 	return i, is, ies
